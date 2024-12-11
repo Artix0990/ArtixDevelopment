f23f08d8b49dd17f4765432a46bd88d7fc08f8db
Artix0990 <evanfalkous101@gmail.com>
2024-12-11 12:02:14 +0000

    resource_lua

---
+++ burgershot/__resource.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Artix'
+description 'Advanced Fast Food Script for QBCore'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script 'config.lua'
+locale 'en'
