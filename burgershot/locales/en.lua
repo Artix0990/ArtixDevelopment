a3374c9d13a662e76ae4bcb1728d2afe7e34683c
Artix0990 <evanfalkous101@gmail.com>
2024-12-11 12:01:16 +0000

    Burgershot free locales
    Burgershot Free by Artix

---
+++ burgershot/locales/en.lua
@@ -0,0 +1,8 @@
+Locales['en'] = {
+    ['transaction_success'] = 'Transaction successful!',
+    ['transaction_failed'] = 'Not enough money!',
+    ['access_fridge'] = 'Accessing fridge storage.',
+    ['using_grill'] = 'Using the grill.',
+    ['using_drinks_machine'] = 'Using the drinks machine.',
+    ['accessing_wholesaler'] = 'Accessing wholesaler.',
+}
